7e7abc3ffa29fb50ccdca30b630c0f69649caf3f
Felipe300 <caracoloro@gmail.com>
2022-05-31 17:39:29 -0400

    Add: change fron HTML and CSS to Astro + Tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'sp': '0px 0px 30px 5px rgba(0, 0, 0, 0.749)',
+			}
+		},
+		colors: {
+			"white": "#ffffff",
+			"Soft-blue": "#8bacda",
+			"Cyan": "#00fff7",
+			"dark-blue-main": "#0d192b",
+			"dark-blue-card": "#14253d",
+			"dark-blue-line": "#2f415b",
+		}
 	},
 	plugins: [],
 }
